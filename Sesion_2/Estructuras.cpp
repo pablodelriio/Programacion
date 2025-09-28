4e7a5e3949578f78c569c3fb1d76254c9de175bd
pablodelriio <pablorm@msmk.university>
2025-09-28 15:50:32 +0200

    Commit 28/09/25

--- Sesion_2/Estructuras.cpp
+++ Sesion_2/Estructuras.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-struct Direccion{
-std::string calle;
-int numero;
-std::string ciudad;
-std::string pais;
-}
-
-Direccion crearDireccion(std::string calle, int numero, std::string ciudad, std::string pais){
-    Direccion dir;
-    dir.calle = calle;
-    dir.numero = numero;
-    dir.ciudad = ciudad;
-    dir.pais = pais;
-    return dir;
-}
+struct Direccion {
+  std::string calle;
+  int numero;
+  std::string ciudad;
+  std::string codigo_postal;
+};
+int main() { 
+    Direccion direccion_casa;
+    direccion_casa.calle = "Avenida Ejemplo";
+    direccion_casa.numero = 42;
+    direccion_casa.ciudad = "Ejemplo City";
+    direccion_casa.codigo_postal = "12345";
+    
+    std::cout << "Calle: " << direccion_casa.calle << std::endl;
+    std::cout << "Numero: " << direccion_casa.numero << std::endl;
+    std::cout << "Ciudad: " << direccion_casa.ciudad << std::endl;
+    std::cout << "Codigo Postal: " << direccion_casa.codigo_postal << std::endl;
+} 
